5335b8e111140411490b1de3e8f42d298c9b9322
C3rb3rus2050 <135362416+C3rb3rus2050@users.noreply.github.com>
2025-08-29 11:12:02 +0200

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,12 +1,3 @@
--- Create database if not exists
-CREATE DATABASE IF NOT EXISTS message_db;
-
--- Create app user that can connect from any host in docker network
-CREATE USER 'appuser'@'%' IDENTIFIED BY 'apppass';
-GRANT ALL PRIVILEGES ON message_db.* TO 'appuser'@'%';
-FLUSH PRIVILEGES;
-
--- Create table
 CREATE TABLE IF NOT EXISTS message (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
