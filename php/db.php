6490f7839d9d4c498560f6a886bb4eab350f3185
C3rb3rus2050 <135362416+C3rb3rus2050@users.noreply.github.com>
2025-08-29 11:54:20 +0200

    Update db.php

--- php/db.php
+++ php/db.php
@@ -4,9 +4,24 @@ $username = "user";
 $password = "userpassword";
 $dbname = "mydb";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$connected = false;
+$tries = 0;
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+while (!$connected && $tries < 10) {
+    try {
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_errno) {
+            throw new Exception($conn->connect_error);
+        }
+        $connected = true;
+    } catch (Exception $e) {
+        $tries++;
+        echo "Waiting for MySQL... attempt $tries<br>";
+        sleep(3); // wait 3 seconds
+    }
+}
+
+if (!$connected) {
+    die("Could not connect to MySQL after several attempts.");
 }
 ?>
